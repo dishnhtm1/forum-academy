8528dc7ddc0b7957870cc26a497ba901187ffc5d
fina202408 <fina202408@st.forum.ac.jp>
2025-05-23 15:38:37 +0900

    updated backend

--- server/routes/applicationRoutes.js
+++ server/routes/applicationRoutes.js
@@ -1,19 +1,7 @@
-// const express = require('express');
-// const { submitApplication } = require('../controllers/applicationController');
-// const router = express.Router();
-
-// router.post('/', submitApplication);
-
-// module.exports = router;
-
 const express = require('express');
+const { submitApplication } = require('../controllers/applicationController');
 const router = express.Router();
-const applicationController = require('../controllers/applicationController');
 
-// Make sure applicationController.createApplication exists and is a function
-router.post('/', applicationController.createApplication);
-
-router.get('/', applicationController.getApplications);
-router.get('/:id', applicationController.getApplicationById);
+router.post('/', submitApplication);
 
 module.exports = router;
